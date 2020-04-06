fd0c6f3abd8e561c54f8a9734da9aa63e3da6182
yeefun <szyeefun@gmail.com>
2020-04-06 23:35:55 +0800

    feat: add ignore-styles comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,5 +24,13 @@ module.exports = {
   ],
   rules: {
     semi: ['error', 'never']
+  },
+  settings: {
+    /**
+     * If you're using some sort of preprocessor on the component styles, then it's likely that when this plugin calls the Svelte compiler on your component, it will throw an exception.
+     * This setting can be given a function that accepts an object of attributes on a <style> tag and returns whether to ignore the style block for the purposes of linting.
+     * see https://github.com/sveltejs/eslint-plugin-svelte3#svelte3ignore-styles
+     */
+    // 'svelte3/ignore-styles': (attributes) => attributes.lang
   }
 }
