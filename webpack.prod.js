ff9bd6e94069b9a80c93f49a60be75a0a94c67fb
yeefun <szyeefun@gmail.com>
2020-04-19 15:50:30 +0800

    feat: add copy-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,21 @@
+const { resolve } = require('path')
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 const webpack = require('webpack')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = merge(common, {
   plugins: [
+    new CopyPlugin([
+      {
+        from: resolve('./public/'),
+        to: resolve('./dist/'),
+        ignore: ['*.html'],
+      },
+    ]),
     new CleanWebpackPlugin(),
     new OptimizeCssAssetsPlugin(),
     new webpack.HashedModuleIdsPlugin()
